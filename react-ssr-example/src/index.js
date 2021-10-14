886d025c920932ee6452d016e9a32d2d9f852e85
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-10-14 11:31:36 -0300

    routing with server-side rendering

--- react-ssr-example/src/index.js
+++ react-ssr-example/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <React.StrictMode>
     <Router>
       <App />
--- react-ssr-example/src/server.js
+++ react-ssr-example/src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import path from 'path';
+import fs from 'fs';
 import { Home } from './pages/Home';
+import App from './App';
 
 const app = express();
 
@@ -9,18 +13,22 @@ app.use(express.static('./build', { index: false }));
 
 app.get('/*', (req, res) => {
     const reactApp = renderToString(
-        <Home />
+        <StaticRouter location={req.url}>
+            <App />
+        </StaticRouter >
     );
 
-    return res.send(`
-        <html>
-            <body>
-                <div id='root'>
-                ${reactApp}
-                </div>
-            </body>
-        </html>
-    `);
+    const templateFile = path.resolve('./build/index.html');
+    fs.readFile(templateFile, 'utf-8', (err, data) => {
+
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        return res.send(
+            data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+        )
+    });
 });
 
 app.listen(8080, () => {
