bff75971e27ca2307cf60fa9db0783be17eb4f35
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-10-19 10:57:50 -0300

    added increment by

--- react-state-management-architecture/src/CounterButton.js
+++ react-state-management-architecture/src/CounterButton.js
@@ -3,11 +3,21 @@ import { useState } from "react";
 export const CounterButton = () => {
 
     const [numberOfClicks, setNumberOfClicks] = useState(0);
+    const [incrementBy, setIncrementBy] = useState(1);
 
     return (
         <>
             <p>You have clicked the button {numberOfClicks} times.</p>
-            <button onClick={() => setNumberOfClicks(numberOfClicks + 1)}>Click</button>
+            <label>
+                Increment By:
+                <input
+                    value={incrementBy}
+                    onChange={e => setIncrementBy(Number(e.target.value))}
+                    type='number'
+                >
+                </input>
+            </label>
+            <button onClick={() => setNumberOfClicks(numberOfClicks + incrementBy)}>Click</button>
         </>
     )
 };
